72132b0e845ff414b720e9823273f876dc340c80
tinmanjk <tinmanjk@mail.com>
2017-08-06 22:32:34 +0300

    added styles from their repo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+//this is included in the <my-app> in the index.html
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { name = 'Angular'; }
